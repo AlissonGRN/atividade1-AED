ea3fd98fdb03f192054cc7fc51a0facbbd85abe8
AlissonGRN <guilhermeribeiro099@gmail.com>
2021-04-26 10:39:00 -0300

    add addClient and searchClient functions

---
+++ models/client/daoClient.py
@@ -0,0 +1,17 @@
+class DaoClient:
+
+    def __init__(self):
+        self.__clients = []
+
+    def addClient(self, client):
+        client = self.searchClient(client.cpf)
+        if client is None:
+            self.__clients.append(client)
+            return True
+        return False
+
+    def searchClient(self, cpf):
+        for client in self.__clients:
+            if client.cpf == cpf:
+                return client
+        return None
